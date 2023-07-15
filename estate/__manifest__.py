0b021474429f34db4668685cf652373edc31b258
thevowels <aungphyohtet1@gmail.com>
2023-07-15 11:38:24 +0630

    On branch master  Changes to be committed: 	modified:   estate/__manifest__.py 	new file:   estate/models/__init__.py 	new file:   estate/models/estate_property.py 	new file:   estate/security/ir.model.access.csv

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,9 +2,10 @@
     "name": "Estate",  # The name that will appear in the App list
     "version": "16.0",  # Version
     "application": True,  # This line says the module is an App, and not a module
-    "depends": ["base"],  # dependencies
+    "depends": ["base"],# dependencies
+    "author": "aph",
     "data": [
-
+        'security/ir.model.access.csv',
     ],
     "installable": True,
     'license': 'LGPL-3',
---
+++ estate/models/estate_property.py
@@ -0,0 +1,21 @@
+from odoo import fields, models
+
+class Estate(models.Model):
+    _name = "estate.property"
+    _description = "Estate property Description"
+
+    name= fields.Char(required = True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required = True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        selection = [('North','North'), ('South', 'South'), ('East','East'), ('West','West')]
+    )
