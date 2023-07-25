f94c9679f68125b1683a2f8435bdecc3ce1cfac7
thevowels <aungphyohtet1@gmail.com>
2023-07-25 17:06:02 +0630

    Before Action Button

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,13 +1,22 @@
-from odoo import fields, models
+from odoo import api,fields, models
 
 class EstateType(models.Model):
     _name="estate.property.type"
     _description="Estate Property Type Description"
     _order = "sequence,name"
+
     name = fields.Char(required=True)
     property_ids = fields.One2many("estate.property", "property_type_id")
     sequence= fields.Integer(default=1)
+    offer_ids = fields.One2many("estate.property.offer","property_type_id")
+    offer_count=fields.Integer(compute="_compute_offer_count")
+
 
     _sql_constraints = [
         ('type_name_unique','UNIQUE(name)','Property types need to have unique name')
     ]
+
+    api.depends("offer_ids")
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids) or 0
