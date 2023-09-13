4c8f6f44435530cde1d6b57017d39fe5bea8b9c2
thevowels <aungphyohtet1@gmail.com>
2023-09-13 20:40:31 +0630

    Finish Chapter 8

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,7 +7,10 @@
         'security/ir.model.access.csv',
 
         'views/estate_property_views.xml',
-        'views/estate_menus.xml'
+        'views/estate_type_views.xml',
+        'views/estate_tag_views.xml',
+        'views/estate_offer_views.xml',
+        'views/estate_menus.xml',
 
     ],
     "installable": True,
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,16 +1,16 @@
-from odoo import models,fields
+from odoo import models, fields
 
 class EstateProperty(models.Model):
     _name = "estate.property"
-    _description= "Model for estate property"
+    _description = "Model for estate property"
 
-    active =fields.Boolean(default=True)
+    active = fields.Boolean(default=True)
 
-    name = fields.Char(required = True)
+    name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date("Available From",copy=False, default = lambda self: fields.Date.today())
-    expected_price = fields.Float(required = True)
+    date_availability = fields.Date("Available From", copy=False, default=lambda self: fields.Date.today())
+    expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
     living_area = fields.Integer("Living Area (sqm)")
@@ -19,18 +19,51 @@ class EstateProperty(models.Model):
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        [('North','North'),
-         ('South','South'),
-         ('East','East'),
-         ('West','West')]
+        [('North', 'North'),
+         ('South', 'South'),
+         ('East', 'East'),
+         ('West', 'West')]
     )
     state = fields.Selection(
         [
-            ('new','New'),
-            ('received','Offer Received'),
-            ('accepted','Offer Accepted'),
-            ('sold','Sold'),
-            ('cancelled','Cancelled'),
+            ('new', 'New'),
+            ('received', 'Offer Received'),
+            ('accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled'),
 
-        ],default='new'
+        ],
+        default='new'
     )
+    type_id = fields.Many2one('estate.property.type', string="Property Type")
+    tag_ids = fields.Many2many('estate.property.tag', string="Tags")
+    user_id = fields.Many2one('res.users', string='Salesman', default=lambda self: self.env.user,)
+    partner_id = fields.Many2one('res.partner', string="Buyer", copy=False)
+
+    offer_ids = fields.One2many('estate.property.offer', 'property_id', "Offers")
+
+class EstatePropertyType(models.Model):
+    _name = "estate.property.type"
+    _description = "Estate Property Type"
+
+    name = fields.Char(required=True)
+
+class EstatePropertyTag(models.Model):
+    _name = "estate.property.tag"
+    _description = "Estate Property Tags"
+
+    name = fields.Char(required=True)
+
+class EstatePropertyOffer(models.Model):
+    _name = "estate.property.offer"
+    _description = "Estate Property Offers"
+
+    price = fields.Float()
+    status = fields.Selection(
+        [('accepted', 'Accepted'),
+         ('refused', 'Refused'),
+        ],
+        copy=False
+    )
+    partner_id = fields.Many2one('res.partner', string="Partner", required=True)
+    property_id = fields.Many2one('estate.property', required=True)
