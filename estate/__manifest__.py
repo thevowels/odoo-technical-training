ec1fe1de243ac5dcdb5df02fdb53ba05ad4eb62e
thevowels <aungphyohtet1@gmail.com>
2023-09-15 23:14:19 +0630

    After Chapter 12

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,9 +7,9 @@
         'security/ir.model.access.csv',
 
         'views/estate_property_views.xml',
+        'views/estate_offer_views.xml',
         'views/estate_type_views.xml',
         'views/estate_tag_views.xml',
-        'views/estate_offer_views.xml',
         'views/estate_menus.xml',
 
     ],
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -4,6 +4,7 @@ from odoo.exceptions import UserError, ValidationError
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Model for estate property"
+    _order = "id desc"
 
     _sql_constraints=[('check_prices','CHECK(expected_price>=0 and selling_price>=0)','Expected and Selling Prices need to be positive')]
 
@@ -20,7 +21,7 @@ class EstateProperty(models.Model):
     living_area = fields.Integer("Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
-    garden = fields.Boolean()
+    garden = fields.Boolean(default=False)
     garden_area = fields.Integer()
     total_area = fields.Integer(compute="_compute_total_area", readonly=True, store=True)
     garden_orientation = fields.Selection(
@@ -44,7 +45,6 @@ class EstateProperty(models.Model):
     tag_ids = fields.Many2many('estate.property.tag', string="Tags")
     user_id = fields.Many2one('res.users', string='Salesman', default=lambda self: self.env.user,)
     partner_id = fields.Many2one('res.partner', string="Buyer", copy=False)
-
     offer_ids = fields.One2many('estate.property.offer', 'property_id', "Offers")
 
     @api.depends("living_area", "garden_area")
@@ -96,22 +96,33 @@ class EstateProperty(models.Model):
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Estate Property Type"
-
+    _order = "name"
     _sql_constraints=[('unique_type_name','UNIQUE(name)','Property Types need to have unique names')]
 
     name = fields.Char(required=True)
+    sequence= fields.Integer('Sequence',default=1,help='helps to maintain order.')
+    property_ids = fields.One2many("estate.property", "type_id")
+    offer_ids = fields.One2many("estate.property.offer", "property_type_id")
+    offer_count = fields.Integer("", compute="_compute_offer_count")
 
+    @api.depends("offer_ids")
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids) or 0
 class EstatePropertyTag(models.Model):
     _name = "estate.property.tag"
     _description = "Estate Property Tags"
+    _order = "name"
 
     _sql_constraints = [('unique_tag_name', 'UNIQUE(name)', 'Property Tag names need to be unique')]
 
     name = fields.Char(required=True)
+    color = fields.Integer()
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
     _description = "Estate Property Offers"
+    _order = "price desc"
 
     _sql_constraints = [('check_price', 'CHECK (price >= 0)','Offers need to have a positive price.')]
 
@@ -124,7 +135,7 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one('res.partner', string="Partner", required=True)
     property_id = fields.Many2one('estate.property', required=True)
-
+    property_type_id = fields.Many2one('estate.property.type',string='Property Type', related='property_id.type_id',store=True)
     validity = fields.Integer(default=7)
     date_deadline = fields.Date( compute="_compute_deadline", inverse="_inverse_deadline", store=True)
 
