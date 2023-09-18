591d6a77bb6e6a74b9f05b641d6504c6d3724bb2
thevowels <aungphyohtet1@gmail.com>
2023-09-18 23:40:58 +0630

    Finish Ch 14

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,11 +1,24 @@
-from odoo import fields, models
+from odoo import Command, fields, models
 
 class EstateProperty(models.Model):
     _inherit="estate.property"
 
     def sold(self):
-        print('inherited_sold method')
-        # account_move = self.env['account.move'].create({'partner_id':'',
-        #                                                 'move_type':'',
-        #                                                 'journal_id':''})
+        for record in self:
+            print('inherited_sold method')
+            self.env['account.move'].create({'partner_id': record.partner_id.id,
+                                             'move_type': 'out_invoice',
+                                             'invoice_line_ids':[
+                                                 Command.create({
+                                                     'name':'Down Payment',
+                                                     'quantity':'1',
+                                                     'price_unit': 0.06 * record.selling_price,
+                                                 }),
+                                                 Command.create({
+                                                     'name': 'Administrative fees',
+                                                     'quantity': '1',
+                                                     'price_unit': 100.00,
+                                                 }),
+                                             ],
+                                             })
         return super().sold()
